48e494d3dcfb6df00104c951f99c368bb4361af6
Artur Cavalcante <artursousac@hotmail.com>
2023-02-24 11:00:19 -0300

    Commit inicial

---
+++ AulasExercicios/src/variaveis/JavaVariaveis.java
@@ -0,0 +1,12 @@
+package variaveis;
+
+public class JavaVariaveis {
+
+	public static void main(String[] args) throws Exception{
+		int numero1 = 1;
+		String numero2 = "2";
+		System.out.println(numero1+numero2);
+
+	}
+
+}
